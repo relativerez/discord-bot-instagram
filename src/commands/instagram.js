b913962303f8b558d8eb50a2e75e6b819e48e09a
relativerez <70732357+relativerez@users.noreply.github.com>
2021-01-29 00:31:40 +0900

    instagram.js

--- src/commands/instagram.js
+++ src/commands/instagram.js
@@ -1,70 +1,33 @@
-exports.run = async (client, message, args, axios, prefix) => {
-  let colorList = [
-    "1752220",
-    "3066993",
-    "3447003",
-    "10181046",
-    "15844367",
-    "15105570",
-    "15158332",
-    "9807270",
-    "8359053",
-    "3426654",
-    "1146986",
-    "2067276",
-    "2123412",
-    "7419530",
-    "12745742",
-    "11027200",
-    "10038562",
-    "9936031",
-    "12370112",
-    "2899536",
-    "16580705",
-    "12320855",
-  ];
-  let colorRand = colorList[Math.floor(Math.random() * colorList.length)];
+require("dotenv").config();
+const Discord = require('discord.js');
+const axios = require('axios');
+const client = new Discord.Client();
 
-  if (!args.length)
-    return message.channel.send(`*Use:* ${"`"}${prefix}instagram @username${"`"}`);
+client.on('warn', console.warn);
+client.on('error', console.error);
+client.on('guildMemberAdd', () => {});
+client.on('guildMemberRemove', () => {});
+client.on('disconnect', () => {});
+client.on('reconnecting', () => { });
+
+client.on('ready', async () => {
+  client.user.setActivity(`${process.env.DISCORD_PREFIX}instagram @user`);
+});
+
+client.on('message', async (message) => {
+
+  const prefix = process.env.PREFIX;
+  const args = message.content.slice(prefix.length).split(' ');
+  const command = args.shift().toLowerCase();
+
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
 
   try {
-    const username = args[0];
-    const response = await axios.get(`https://www.instagram.com/${username}/?__a=1`);
-    const { data } = response;
-
-    await message.channel.send({
-      embed: {
-        color: colorRand,
-        author: {
-          name: `${data["graphql"]["user"].full_name} (@${username})`,
-        },
-        thumbnail: {
-          url: data["graphql"]["user"].profile_pic_url,
-        },
-        fields: [
-          {
-            name: "Seguidores",
-            value: data["graphql"]["user"]["edge_followed_by"].count,
-            inline: true,
-          },
-          {
-            name: "Seguindo",
-            value: data["graphql"]["user"]["edge_follow"].count,
-            inline: true,
-          },
-          {
-            name: "Biografia",
-            value: data["graphql"]["user"].biography,
-          },
-        ],
-        //footer: {
-        //text: `Criado por Acacio De Lima`
-        //icon_url: 'https://i.imgur.com/1nrsIPc.png'
-        //},
-      },
-    });
-  } catch (error) {
-    message.channel.send("Usuário não encontrado.");
+    let commandFile = require(`./src/commands/${command}.js`);
+    commandFile.run(client, message, args, axios, prefix);
+  } catch (e) {
+    console.log(e.stack);
   }
-};
+});
+
+client.login(process.env.DISCORD_TOKEN);
